268f781572bb6b93d1e4a1eba85497d700d442ff
hakan <kokarcalihakan@gmail.com>
2020-04-19 08:57:47 +0300

    finaldemo

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+    public GameObject prefab;
+    public float requiredEmptyRadius;
+    public UnityEngine.UI.Text clicktoStart;
+    const int Left = 0;
+
+    void Update() {
+        if (Input.GetMouseButtonDown(Left)) {
+
+            clicktoStart.enabled = false;
+
+            Vector2 position =
+                Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (!Physics2D.OverlapCircle(position, requiredEmptyRadius)) {
+                Instantiate(prefab, position, Quaternion.identity);
+            }
+        }
+    }
+
+
+}
